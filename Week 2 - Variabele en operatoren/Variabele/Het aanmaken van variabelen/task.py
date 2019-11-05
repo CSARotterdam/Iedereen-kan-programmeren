978444af9b6a1e291004411a880adf89833a63cc
Quinten <Q.verheij@hotmail.com>
2019-11-05 18:09:35 +0100

    Added first task in old structure
    tested it too

--- Week 2 - Variabele en operatoren/Variabele/Het aanmaken van variabelen/task.py
+++ Week 2 - Variabele en operatoren/Variabele/Het aanmaken van variabelen/task.py
@@ -1,3 +1,49 @@
-# todo: replace this with an actual task
-def sum(a, b):
-    return a + b
+## Opdracht 1: Vul de verschillende velden in met de juiste data.
+
+x = 5
+#Variabele naam: x
+#Variabele waarde: 5
+#Variabele type: integer
+
+y = 235
+#Variabele naam: y
+#Variabele waarde: 235
+#Variabele type: integer
+
+z = -721
+#Variabele naam: z
+#Variabele waarde: -721
+#Variabele type: integer
+
+
+stappenteller = 0
+#Variabele naam: stappenteller
+#Variabele waarde: 0
+#Variabele type: integer
+
+tijdelijkGetal = 45
+#Variabele naam: tijdelijkGetal
+#Variabele waarde: 45
+#Variabele type: integer
+
+aantalHoeken = 4
+#Variabele naam: aantalHoeken
+#Variabele waarde: 4
+#Variabele type: integer
+
+
+varA = -342
+#Variabele naam: varA
+#Variabele waarde: -342
+#Variabele type: integer
+
+varB = 244
+#Variabele naam: varB
+#Variabele waarde: 244
+#Variabele type: integer
+
+varC = 8942
+#Variabele naam: varC
+#Variabele waarde: 8942
+#Variabele type: integer
+
