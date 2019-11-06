3977d2e9c1223276c346eaba524d5da3457ff440
Quinten Verheij <Q.verheij@hotmail.com>
2019-11-06 14:13:40 +0100

    Reverted week 3 to old structure

--- Week 3 - Branching/If/Programma 1/tests/test_task.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from regex_helper import regex_helper
-
-
-class TestCase(unittest.TestCase):
-    def test(self):
-        a = regex_helper()
-        
-        self.assertEqual(1, 0, msg="Implement tests")
