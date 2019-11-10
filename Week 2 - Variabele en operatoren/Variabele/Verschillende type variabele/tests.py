f7381def47e00c9bf98ef5111afc2150028868a7
Quinten <Q.verheij@hotmail.com>
2019-11-10 18:41:48 +0100

    Added tests for 2nd task

--- Week 2 - Variabele en operatoren/Variabele/Verschillende type variabele/tests.py
+++ Week 2 - Variabele en operatoren/Variabele/Verschillende type variabele/tests.py
@@ -1,48 +1,69 @@
-from test_helper import run_common_tests, failed, passed, get_answer_placeholders
-from regex_helper import *
 from UserInputHelper import *
+from test_helper import run_common_tests, failed, passed
+import unittest
 
-def test_answer_placeholders():
-    helper = Helper()
-    for answerNumber in range(len(helper.answers)):
-        outcome = False
-        backupText = ""
-        if(answerNumber == 0):
-            outcome = helper.CheckInput(answerNumber, "x")
-        elif(answerNumber == 1):
-            outcome = helper.CheckInput(answerNumber, "235")
-        elif(answerNumber == 2):
-            outcome = helper.CheckInput(answerNumber, "integer|Integer")
-        elif(answerNumber == 3):
-            outcome = helper.CheckInput(answerNumber, "stappenteller")
-        elif(answerNumber == 4):
-            outcome = helper.CheckInput(answerNumber, "0")
-        elif(answerNumber == 5):
-            outcome = helper.CheckInput(answerNumber, "45")
-        elif(answerNumber == 6):
-            outcome = helper.CheckInput(answerNumber, "integer|Integer")
-        elif(answerNumber == 7):
-            outcome = helper.CheckInput(answerNumber, "aantalHoeken")
-        elif(answerNumber == 8):
-            outcome = helper.CheckInput(answerNumber, "4")
-        elif(answerNumber == 9):
-            outcome = helper.CheckInput(answerNumber, "integer|Integer")
-        elif(answerNumber == 10):
-            outcome = helper.CheckInput(answerNumber, helper.re.expressionToRegex("varA = -342"))
-        elif(answerNumber == 11):
-            outcome = helper.CheckInput(answerNumber, helper.re.expressionToRegex("varB = 244"))
-        elif(answerNumber == 12):
-            outcome = helper.CheckInput(answerNumber, helper.re.expressionToRegex("varC = 8942"))
-        CheckOutcome(outcome, answerNumber, backupText)
+helper = Helper()
 
-def CheckOutcome(outcome, answerNumber, backupText):
-    if(outcome):
-        passed("Answer #" + str(answerNumber) + " is correct!")
-    else:
-        failed("There is an error in answer #"+ str(answerNumber)+" " + backupText)
+
+class varAanmaken(unittest.TestCase):
+    def testAnswer1(self):
+        self.assertEqual(helper.answers[0], "varA")
+
+    def testAnswer2(self):
+        self.assertEqual(helper.answers[1], "\"string\"")
+
+    def testAnswer3(self):
+        self.assertEqual(helper.answers[2], "\"stringString\"")
+
+    def testAnswer4(self):
+        self.assertRegex(helper.answers[3], "string|string")
+
+    def testAnswer5(self):
+        self.assertEqual(helper.answers[4], "varC")
+
+    def testAnswer6(self):
+        self.assertEqual(helper.answers[5], "0")
+
+    def testAnswer7(self):
+        self.assertRegex(helper.answers[6], "integer|Integer|int")
+
+    def testAnswer8(self):
+        self.assertEqual(helper.answers[7], "varD")
+
+    def testAnswer9(self):
+        self.assertEqual(helper.answers[8], "1")
+
+    def testAnswer10(self):
+        self.assertRegex(helper.answers[9], "integer|Integer|int")
+
+    def testAnswer11(self):
+        self.assertRegex(helper.answers[10], helper.re.expressionToRegex("varE = 145.1"))
+
+    def testAnswer12(self):
+        self.assertEqual(helper.answers[11], "23.5345")
+
+    def testAnswer13(self):
+        self.assertRegex(helper.answers[12], "float|Float")
+
+    def testAnswer14(self):
+        self.assertRegex(helper.answers[13], helper.re.expressionToRegex("varG = True"))
+
+    def testAnswer15(self):
+        self.assertEqual(helper.answers[14], "varH")
+
+    def testAnswer16(self):
+        self.assertEqual(helper.answers[15], "False")
+
+    def testAnswer17(self):
+        self.assertRegex(helper.answers[16], "boolean|Boolean")
 
 if __name__ == '__main__':
-    run_common_tests()
-    test_answer_placeholders()
+    suite = unittest.TestLoader().loadTestsFromTestCase(testCaseClass=varAanmaken)
+    res = unittest.TextTestRunner().run(suite)
+    if res.wasSuccessful():
+        passed("Congratulations")
+    else:
+        for el in res.failures:
+            failed(f"There is an error in {el[0]}")
 
 
