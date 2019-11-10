00f5826ff114231945b09f726782cdf119a8e219
Quinten <Q.verheij@hotmail.com>
2019-11-10 19:33:47 +0100

    Removed eindopdrachten

--- Eindopdrachten - Python/Opdracht 1/Omdraaien van tekst/task.py
+++
@@ -1,9 +0,0 @@
-def run():
-    textInput = input("Enter the text you would like to reverse:\n")
-    reversedText = reverse(textInput)
-    print("Your reversed string is:\n" + reversedText)
-
-def reverse(text):
-    return "Nope"
-
-run()
